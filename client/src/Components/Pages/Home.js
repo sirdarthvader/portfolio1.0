8230b8e7190f629218237daa5ebac8ec51d1223b
Ashish Singh <ashishcodes4@gmail.com>
2018-12-24 01:25:35 +0530

    deploy 5

---
+++ client/src/Components/Pages/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className='home'>
+        <div className="jumbotron">
+        <h1 className="display-1">Hello, world!</h1>
+        <h5  style={{color: 'white'}}>I am Ashish, I am a javaScript developer.</h5>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ client/src/Components/Pages/Skills.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+class Skills extends Component {
+  render() {
+    return (
+      <div className="skills">
+        <div className="list">
+          <div>
+           <i className="devicon-javascript-plain colored" />
+          </div>
+          <div>
+            <i className="devicon-nodejs-plain-wordmark colored" />
+          </div>
+          <div>
+            <i className="devicon-express-original colored" />
+          </div>
+          <div>
+            <i className="devicon-mongodb-plain-wordmark colored" />
+          </div>
+          <div>
+            <i className="devicon-babel-plain colored" />
+          </div>
+          <div>
+            <i className="devicon-webpack-plain-wordmark colored" />
+          </div>
+          <div>
+            <i className="devicon-html5-plain-wordmark colored" />
+          </div>
+          <div>
+            <i className="devicon-css3-plain-wordmark colored" />
+          </div>
+          <div>
+            <i className="devicon-git-plain-wordmark colored" />
+          </div>
+          <div>
+            <i className="devicon-github-plain colored" />
+          </div>
+          <div>
+            <i className="devicon-jquery-plain-wordmark colored" />
+          </div>
+          <div>
+            <i className="devicon-visualstudio-plain-wordmark colored" />
+          </div>
+          <div>
+            <i className="devicon-typescript-plain colored" />
+          </div>
+          <div>
+            <i className="devicon-d3js-plain" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Skills;
