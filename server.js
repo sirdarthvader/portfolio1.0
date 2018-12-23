33b92cd2dc469a16b0af3603d590d8bde56b0b5b
Ashish Singh <ashishcodes4@gmail.com>
2018-12-23 23:59:34 +0530

    deploy 3

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 //Static file declaration
 app.use(express.static(path.join(__dirname, 'client/build')));
