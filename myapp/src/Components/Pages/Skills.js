73609a0f0da56f5f4314d9bd81697ffca9812dd1
Ashish Singh <ashishcodes4@gmail.com>
2018-12-23 23:10:28 +0530

    completed working on portfolio

--- myapp/src/Components/Pages/Skills.js
+++ myapp/src/Components/Pages/Skills.js
@@ -6,46 +6,46 @@ class Skills extends Component {
       <div className="skills">
         <div className="list">
           <div>
-            <i class="devicon-javascript-plain colored" />
+           <i className="devicon-javascript-plain colored" />
           </div>
           <div>
-            <i class="devicon-nodejs-plain-wordmark colored" />
+            <i className="devicon-nodejs-plain-wordmark colored" />
           </div>
           <div>
-            <i class="devicon-express-original colored" />
+            <i className="devicon-express-original colored" />
           </div>
           <div>
-            <i class="devicon-mongodb-plain-wordmark colored" />
+            <i className="devicon-mongodb-plain-wordmark colored" />
           </div>
           <div>
-            <i class="devicon-babel-plain colored" />
+            <i className="devicon-babel-plain colored" />
           </div>
           <div>
-            <i class="devicon-webpack-plain-wordmark colored" />
+            <i className="devicon-webpack-plain-wordmark colored" />
           </div>
           <div>
-            <i class="devicon-html5-plain-wordmark colored" />
+            <i className="devicon-html5-plain-wordmark colored" />
           </div>
           <div>
-            <i class="devicon-css3-plain-wordmark colored" />
+            <i className="devicon-css3-plain-wordmark colored" />
           </div>
           <div>
-            <i class="devicon-git-plain-wordmark colored" />
+            <i className="devicon-git-plain-wordmark colored" />
           </div>
           <div>
-            <i class="devicon-github-plain colored" />
+            <i className="devicon-github-plain colored" />
           </div>
           <div>
-            <i class="devicon-jquery-plain-wordmark colored" />
+            <i className="devicon-jquery-plain-wordmark colored" />
           </div>
           <div>
-            <i class="devicon-visualstudio-plain-wordmark colored" />
+            <i className="devicon-visualstudio-plain-wordmark colored" />
           </div>
           <div>
-            <i class="devicon-typescript-plain colored" />
+            <i className="devicon-typescript-plain colored" />
           </div>
           <div>
-            <i class="devicon-d3js-plain" />
+            <i className="devicon-d3js-plain" />
           </div>
         </div>
       </div>
