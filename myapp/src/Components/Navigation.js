83ffaf93eb760dd5b820113b14daf4bef1604b31
Ashish Singh <ashishcodes4@gmail.com>
2018-12-23 18:24:38 +0530

    background added

---
+++ myapp/src/Components/Navigation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Navigation extends Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{color: 'white'}}>This is the navigation bar</h1>
+      </div>
+    )
+  }
+}
+
+export default Navigation;
