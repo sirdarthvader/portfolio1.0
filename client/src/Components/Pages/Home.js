d22b6f98f7570f6ebf6d198eabbd1d0b34af8ed6
Ashish Singh <ashishcodes4@gmail.com>
2018-12-25 01:09:14 +0530

    wokring on tying effect

--- client/src/Components/Pages/Home.js
+++ client/src/Components/Pages/Home.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Typing from "react-typing-animation";
 
 class Home extends Component {
   render() {
     return (
-      <div className='home'>
+      <div className="home">
         <div className="jumbotron">
-        <h1 className="display-1">Hello, world!</h1>
-        <h5  style={{color: 'white'}}>I am Ashish, I am a javaScript developer.</h5>
+          <h1 className="display-1">Hello, world!</h1>
+          <h5 style={{ color: "white" }}>
+            I am Ashish, I am a javaScript developer.
+          </h5>
+          <Typing>
+            <span>This span will get typed.</span>
+          </Typing>
         </div>
       </div>
     );
