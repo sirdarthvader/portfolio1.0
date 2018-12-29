b4fd71535c3c0241a511e3a7c4d1a4a77632cb7a
Ashish Singh <ashishcodes4@gmail.com>
2018-12-29 07:45:01 +0530

    able to toggle background on button click

--- client/src/App.js
+++ client/src/App.js
@@ -9,153 +9,149 @@ import Blog from './Components/Pages/Blog'
 import Skills from './Components/Pages/Skills'
 import Contact from './Components/Pages/Contact'
 import TwitterFeed from './Components/Pages/TwitterFeed'
-import BgSelector from './Components/Bits/BgSelector';
 
-const snowParticle = {
-  particles: {
-    number: {
-      value: 160,
-      density: {
-        enable: false
-      }
-    },
-    size: {
-      value: 3,
-      random: true,
-      anim: {
-        speed: 4,
-        size_min: 0.3
-      }
-    },
-    line_linked: {
-      enable: false
-    },
-    move: {
-      random: true,
-      speed: 1,
-      direction: 'top',
-      out_mode: 'out'
-    }
-  },
-  interactivity: {
-    events: {
-      onhover: {
-        enable: true,
-        mode: 'bubble'
-      },
-      onclick: {
-        enable: true,
-        mode: 'repulse'
-      }
-    },
-    modes: {
-      bubble: {
-        distance: 250,
-        duration: 2,
-        size: 0,
-        opacity: 0
-      },
-      repulse: {
-        distance: 400,
-        duration: 4
-      }
-    }
-  }
-}
 
-const starParticles = {
-  particles: {
-    number: {
-      value: 50
-    },
-    size: {
-      value: 3
-    }
-  },
-  interactivity: {
-    events: {
-      onhover: {
-        enable: true,
-        mode: 'repulse'
-      }
-    }
-  }
-}
-
-const bublesParticle = {
-  particles: {
-    number: {
-      value: 160,
-      density: {
-        enable: false
-      }
-    },
-    size: {
-      value: 10,
-      random: true
-    },
-    move: {
-      direction: 'bottom',
-      out_mode: 'out'
-    },
-    line_linked: {
-      enable: false
-    }
-  },
-  interactivity: {
-    events: {
-      onclick: {
-        enable: true,
-        mode: 'remove'
-      }
-    },
-    modes: {
-      remove: {
-        particles_nb: 10
-      }
-    }
-  }
-}
-
-let particleOptions;
-
-// switch (this.state.particleOptions) {
-//   case 'snow':
-//     particleOptions = snowParticle
-//     break
-
-//   case 'star':
-//     particleOptions = starParticles
-//     break
-
-//   case 'bubbles':
-//     particleOptions = bublesParticle
-//     break
-
-//   default:
-//     particleOptions = starParticles
-// }
 
 class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      particleOptions: ''
+      bgOption: ''
     }
-  }
-  
-  onclick (e) {
-    e.preventDefault();
-    
+    this.onclick = this.onclick.bind(this);
   }
 
-  
+  onclick (e) {
+    e.preventDefault();
+    this.setState({bgOption: e.target.name});
+  }
+
   render () {
+    const {bgOption} = this.state;
+
+    const snowParticle = {
+      particles: {
+        number: {
+          value: 160,
+          density: {
+            enable: false
+          }
+        },
+        size: {
+          value: 3,
+          random: true,
+          anim: {
+            speed: 4,
+            size_min: 0.3
+          }
+        },
+        line_linked: {
+          enable: false
+        },
+        move: {
+          random: true,
+          speed: 1,
+          direction: 'top',
+          out_mode: 'out'
+        }
+      },
+      interactivity: {
+        events: {
+          onhover: {
+            enable: true,
+            mode: 'bubble'
+          },
+          onclick: {
+            enable: true,
+            mode: 'repulse'
+          }
+        },
+        modes: {
+          bubble: {
+            distance: 250,
+            duration: 2,
+            size: 0,
+            opacity: 0
+          },
+          repulse: {
+            distance: 400,
+            duration: 4
+          }
+        }
+      }
+    }
+    
+    const starParticle = {
+      particles: {
+        number: {
+          value: 50
+        },
+        size: {
+          value: 3
+        }
+      },
+      interactivity: {
+        events: {
+          onhover: {
+            enable: true,
+            mode: 'repulse'
+          }
+        }
+      }
+    }
+    
+    const bubbleParticle = {
+      particles: {
+        number: {
+          value: 160,
+          density: {
+            enable: false
+          }
+        },
+        size: {
+          value: 10,
+          random: true
+        },
+        move: {
+          direction: 'bottom',
+          out_mode: 'out'
+        },
+        line_linked: {
+          enable: false
+        }
+      },
+      interactivity: {
+        events: {
+          onclick: {
+            enable: true,
+            mode: 'remove'
+          }
+        },
+        modes: {
+          remove: {
+            particles_nb: 10
+          }
+        }
+      }
+    }
+    
+    let particleOptions;
+
+    if(bgOption === 'star') {
+      particleOptions = <Particles className='background' params={starParticle} />
+    } else {
+      if(bgOption === 'bubble') {
+        particleOptions = <Particles className='background' params={bubbleParticle} />
+      } else {
+        particleOptions = <Particles className='background' params={snowParticle} />
+      }
+    }
+    
     return (
       <Router>
         <div className='container'>
           <div className='App'>
-            <Particles className='background' params={particleOptions} />
+            {particleOptions}
             <Navigation />
             <Route exact path='/' component={Home} />
             <Route exact path='/projects' component={Projects} />
@@ -163,7 +159,19 @@ class App extends Component {
             <Route exact path='/skills' component={Skills} />
             <Route exact path='/contact' component={Contact} />
             <Route exact path='/twitter' component={TwitterFeed} />
-            <BgSelector />
+            <div className='bg-buttons'>
+              <div className='box'>
+                <button className='btn btn-primary mb-2' name='snow' onClick={this.onclick}>
+                  Snow
+                </button>
+                <button className='btn btn-primary mb-2' name='star' onClick={this.onclick}>
+                  Star
+                </button>
+                <button className='btn btn-primary mb-2' name='bubble' onClick={this.onclick}>
+                  Bubble
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </Router>
